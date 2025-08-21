d4ef8fe457590c110855625bed408ab04d0059d5
Krishnamoorthy0430 <krishnamoorthy0704@gmail.com>
2025-08-22 00:04:35 +0530

    Add environment variable support and update .gitignore

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello CI/CD World 🚀"}
+    return {
+        "message": "Hello CI/CD World 🚀",
+        "environment": os.getenv("APP_ENV", "unknown")
+        }
